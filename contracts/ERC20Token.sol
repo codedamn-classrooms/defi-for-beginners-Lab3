71ed0354d0c527871dc4b4a8f1ff9897092785dc
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-04 14:34:52 +0530

    update sol

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -16,66 +16,6 @@ interface IERC20 {
 
 contract ERC20Token is IERC20 { 
 
-    string public name; 
-    string public symbol; 
-    uint8 public decimals; 
-    uint256 public _totalSupply; 
-    mapping(address => uint256) public totalBalance; 
-    mapping(address => mapping(address => uint256)) public allow;
-
-    constructor(string memory _name,string memory _symbol,uint8 _decimals,uint256 __totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        _totalSupply = __totalSupply;
-        totalBalance[msg.sender] = _totalSupply;
-    }
-
-    function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
-    }
-    
-    function balanceOf(address addr) public view override returns (uint256) {
-        return totalBalance[addr];
-    }
-
-    function transfer(address to, uint256 amount)public override returns (bool){
-    require(totalBalance[msg.sender] >= amount, "not sufficient balance");
-    totalBalance[msg.sender] -= amount;
-    totalBalance[to] += amount;
-    emit Transfer(msg.sender, to, amount);
-    return true;
-}
-
-     
-    function approve(address spender, uint256 amount) public override returns (bool){
-        require(totalBalance[msg.sender]>=amount);
-        require(amount>0);
-        allow[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view override returns (uint256){
-        return allow[owner][spender];
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        require(allow[from][msg.sender] >= amount, "allowance too low");
-        require(totalBalance[from] >= amount, "amount to be sent should be less than total balance");
-        allow[from][msg.sender] -= amount; 
-        totalBalance[from] -= amount;
-        totalBalance[to] += amount;
-        emit Transfer(from, to, amount);
-        return true;
-    }
-
-    
-
-    
-
-    
-
-    
+//Write code here
 
 }
